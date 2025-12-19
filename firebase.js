7e4ba7446da17f59cec592c1f810413cbbb8ec53
ivanadepiyo <ivanadepiyo11@gmail.com>
2025-12-19 23:29:48 +0900

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,6 @@
-// Import Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
-// Konfigurasi Firebase (ISI DARI FIREBASE KAMU)
 const firebaseConfig = {
   apiKey: "ISI_API_KEY_KAMU",
   authDomain: "ISI_AUTH_DOMAIN",
@@ -12,11 +10,7 @@ const firebaseConfig = {
   appId: "ISI_APP_ID"
 };
 
-// Inisialisasi Firebase
 const app = initializeApp(firebaseConfig);
-
-// Firestore database
 const db = getFirestore(app);
 
-// Export db supaya bisa dipakai di script.js
 export { db };
