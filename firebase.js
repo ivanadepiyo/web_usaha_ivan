9e98ac0084c00c9001c0de599a20e2b2b1929917
ivanadepiyo <ivanadepiyo11@gmail.com>
2025-12-20 00:01:55 +0900

    firebase.js

--- firebase.js
+++ firebase.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBjDHapatmMS4C2EDFXxp_WQXVhb0UCZZg",
   authDomain: "web-usaha-ivan.firebaseapp.com",
   projectId: "web-usaha-ivan",
-  storageBucket: "web-usaha-ivan.firebasestorage.app",
+  storageBucket: "web-usaha-ivan.appspot.com",
   messagingSenderId: "588839561784",
   appId: "1:588839561784:web:4accd7d9316a5a0ec5d4cd"
 };
