6850ec53a33d209aa242bd13b86b8f2a15ce6fce
ivanadepiyo <ivanadepiyo11@gmail.com>
2025-12-21 20:50:00 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,19 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBjDHapatmMS4C2EDFXxp_WQXVhb0UCZZg",
-  authDomain: "web-usaha-ivan.firebaseapp.com",
-  projectId: "web-usaha-ivan",
-  storageBucket: "web-usaha-ivan.appspot.com",
-  messagingSenderId: "588839561784",
-  appId: "1:588839561784:web:4accd7d9316a5a0ec5d4cd"
-};
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-auth-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js"></script>
 
-firebase.initializeApp(firebaseConfig);
+<script>
+  const firebaseConfig = {
+    apiKey: "AIzaSyBjDHapatmMS4C2EDFXxp_WQXVhb0UCZZg",
+    authDomain: "web-usaha-ivan.firebaseapp.com",
+    projectId: "web-usaha-ivan",
+    storageBucket: "web-usaha-ivan.appspot.com",
+    messagingSenderId: "588839561784",
+    appId: "1:588839561784:web:4accd7d9316a5a0ec5d4cd"
+  };
 
-const auth = firebase.auth();
-const db = firebase.firestore();
+  firebase.initializeApp(firebaseConfig);
+
+  const auth = firebase.auth();
+  const db = firebase.firestore();
+</script>
