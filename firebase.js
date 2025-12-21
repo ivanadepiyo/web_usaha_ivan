dd10643be567ea15d2dbd6c88a3c294b34723e8b
ivanadepiyo <ivanadepiyo11@gmail.com>
2025-12-21 20:38:36 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
-
 const firebaseConfig = {
   apiKey: "AIzaSyBjDHapatmMS4C2EDFXxp_WQXVhb0UCZZg",
   authDomain: "web-usaha-ivan.firebaseapp.com",
@@ -11,7 +7,7 @@ const firebaseConfig = {
   appId: "1:588839561784:web:4accd7d9316a5a0ec5d4cd"
 };
 
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const auth = firebase.auth();
+const db = firebase.firestore();
