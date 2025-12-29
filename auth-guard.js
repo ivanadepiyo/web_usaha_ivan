9725ae15672fed9e5b62806e1d6a33fd7e07fd5a
ivanadepiyo <ivanadepiyo11@gmail.com>
2025-12-29 13:57:15 +0900

    Update auth-guard.js

--- auth-guard.js
+++ auth-guard.js
@@ -1,7 +1,17 @@
-<script>
-  auth.onAuthStateChanged(user => {
-    if (!user) {
-      window.location.href = "login.html";
-    }
-  });
-</script>
+import { signInWithEmailAndPassword } from 
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { auth } from "./firebase.js";
+
+function login() {
+  const email = email.value;
+  const password = password.value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      msg.innerText = "✅ Login berhasil";
+      location.href = "dashboard.html";
+    })
+    .catch(() => {
+      msg.innerText = "❌ Email atau password salah";
+    });
+}
