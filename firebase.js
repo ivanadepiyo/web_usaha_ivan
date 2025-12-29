ddab321c47126447f83ec7beef647a4850dc7393
ivanadepiyo <ivanadepiyo11@gmail.com>
2025-12-29 13:55:24 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,13 @@
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-auth-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js"></script>
+<script type="module">
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-<script>
   const firebaseConfig = {
     apiKey: "AIzaSyBjDHapatmMS4C2EDFXxp_WQXVhb0UCZZg",
     authDomain: "web-usaha-ivan.firebaseapp.com",
     projectId: "web-usaha-ivan",
-    storageBucket: "web-usaha-ivan.appspot.com",
-    messagingSenderId: "588839561784",
-    appId: "1:588839561784:web:4accd7d9316a5a0ec5d4cd"
   };
 
-  firebase.initializeApp(firebaseConfig);
-
-  const auth = firebase.auth();
-  const db = firebase.firestore();
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 </script>
