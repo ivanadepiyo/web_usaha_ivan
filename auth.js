ab399a53d92dd9ed15435090440bcd77d1e169f8
ivanadepiyo <ivanadepiyo11@gmail.com>
2025-12-30 12:10:54 +0900

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,30 +1,33 @@
-<!-- auth.js -->
 <script>
-function login(email, password) {
+const auth = firebase.auth();
+
+function login(email, password){
   auth.signInWithEmailAndPassword(email, password)
-    .then(() => window.location.href = "dashboard.html")
-    .catch(err => alert(err.message));
+    .then(userCredential=>{
+      if(!userCredential.user.emailVerified){
+        alert("Silahkan verifikasi email dulu!");
+        auth.signOut();
+        return;
+      }
+      window.location.href = "dashboard.html";
+    })
+    .catch(err=>alert(err.message));
 }
 
-function register(email, password) {
-  auth.createUserWithEmailAndPassword(email, password)
-    .then(() => alert("Registrasi berhasil"))
-    .catch(err => alert(err.message));
+function logout(){
+  auth.signOut().then(()=> window.location.href="login.html");
 }
 
-function logout() {
-  auth.signOut().then(() => window.location.href = "login.html");
-}
-
-function resetPassword(email) {
+function resetPassword(email){
   auth.sendPasswordResetEmail(email)
-    .then(() => alert("Link reset terkirim"))
-    .catch(err => alert(err.message));
+    .then(()=> alert("Link reset password terkirim"))
+    .catch(err=> alert(err.message));
 }
 
-auth.onAuthStateChanged(user => {
-  if (!user && location.pathname.includes("dashboard")) {
-    window.location.href = "login.html";
+// Proteksi halaman dashboard
+auth.onAuthStateChanged(user=>{
+  if(!user && location.pathname.includes("dashboard")){
+    window.location.href="login.html";
   }
 });
 </script>
